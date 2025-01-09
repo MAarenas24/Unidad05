c3e8190a84eb73b5b5c03012580eb64446953c2f
Miguel Ángel Arenas Cuevas <168757213+MAarenas24@users.noreply.github.com>
2025-01-09 17:30:59 +0100

    Add files via upload

---
+++ Ejemplo02Matrices.java
@@ -0,0 +1,45 @@
+package unidad05;
+
+/**
+ *
+ * @author Miguel Angel Arenas
+ */
+public class Ejemplo02Matrices {
+    
+    public static void main(String[] args) {
+        
+        int[][] notas = new int[5][6];
+        
+        notas[2][3] = 8;
+        
+        System.out.println("notas[0][0] = " + notas[0][0]);
+        System.out.println("notas[2][3] = " + notas[2][3]);
+        
+        System.out.println("-------------------------");
+        
+        imprimirMatriz(notas);
+        rellenarMatriz(notas, -9);
+        imprimirMatriz(notas);
+        
+    }
+    
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            for (int nota : fila) {
+                System.out.print(nota + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------");
+        System.out.println();
+    }
+    
+    public static void rellenarMatriz(int[][] matriz, int defaultValue) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = defaultValue;
+            }
+        }
+    }
+    
+}
