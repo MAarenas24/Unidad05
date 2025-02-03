36f64b7e5e292152ed4bc091b5b800e554aae670
Miguel Ángel Arenas Cuevas <168757213+MAarenas24@users.noreply.github.com>
2025-02-03 17:30:03 +0100

    Add files via upload

--- ejercicios/ejercicioClase01/Main.java
+++ ejercicios/ejercicioClase01/Main.java
@@ -7,7 +7,10 @@ import java.util.Arrays;
  * @author tarde
  */
 public class Main {
-    
+    static final int LIMITE_POSICIONES_VACIAS = 10;
+    static final int POS_VACIAS_DEFAULT = 5;
+    int[] array;
+    int arrayLength;
     public static void main(String[] args) {
         
         MiArray a = new MiArray(7);
@@ -26,5 +29,33 @@ public class Main {
         
         a.insertarEn(7, 5);
     }
+    
+//    public String toString() {
+//        StringBuilder msg = new StringBuilder("[");
+//        for (int i = 0; i < arrayLength; i++) {
+//            msg.append(arrays[i]);
+//            if (i < arrayLength - 1) msg.append(", ");
+//        }
+//        msg.append("]");
+//        return msg.toString();
+//    }
+    
+//    public static void acortarArray() {
+//        if (isEmpty) {
+//            return;
+//        }
+//        int numPosLibres = array.length - arrayLength;
+//        if (numPosLibres >= LIMITE_POSICIONES_VACIAS) {
+//            int[] arrayAux = new int[arrayLength + POS_VACIAS_DEFAULT]; 
+//            for (int i = 0; i < arrayLength; i++) {
+//                arrayAux[i] = array[i];
+//            }
+//            array = arrayAux;
+//        }
+//    }
+    
+//    public static boolean isEmpty() {
+//        return (array != null) && (arrayLength == 0);
+//    }
     
 }
