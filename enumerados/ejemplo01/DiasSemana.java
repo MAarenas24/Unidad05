31877cff0d7147dca5b3a68e18856d8526534d89
Miguel Ángel Arenas Cuevas <168757213+MAarenas24@users.noreply.github.com>
2025-02-05 18:12:55 +0100

    Add files via upload

--- enumerados/ejemplo01/DiasSemana.java
+++ enumerados/ejemplo01/DiasSemana.java
@@ -5,20 +5,24 @@ package unidad05.enumerados.ejemplo01;
  * @author Miguel Angel Arenas
  */
 public enum DiasSemana {
-    LUNES("Lunes", "lun"),
-    MARTES("Martes", "mar"),
-    MIERCOLES("Miercoles", "mie"),
-    JUEVES("Jueves", "jue"),
-    VIERNES("Viernes", "vie"),
-    SABADO("Sabado", "sab"),
-    DOMINGO("Domingo", "dom");
+    LUNES("Lunes", "lun", "L", "Es laboral"),
+    MARTES("Martes", "mar", "M", "Es laboral"),
+    MIERCOLES("Miercoles", "mie", "X", "Es laboral"),
+    JUEVES("Jueves", "jue", "J", "Es laboral"),
+    VIERNES("Viernes", "vie", "V", "Es laboral"),
+    SABADO("Sabado", "sab", "S", "No es laboral"),
+    DOMINGO("Domingo", "dom", "D", "No es laboral");
     
     private final String nombre;
     private final String nombreCorto;
+    private final String abreviacion;
+    private final String diaLaboral; 
 
-    private DiasSemana(String nombre, String nombreCorto) {
+    private DiasSemana(String nombre, String nombreCorto, String abreviacion, String diaLaboral) {
         this.nombre = nombre;
         this.nombreCorto = nombreCorto;
+        this.abreviacion = abreviacion;
+        this.diaLaboral = diaLaboral;
     }
     
     public String getNombre() {
@@ -28,5 +32,14 @@ public enum DiasSemana {
     public String getNombreCorto() {
         return nombreCorto;
     }
+    
+    public String getAbreviacion() {
+        return abreviacion;
+    }
+
+    public String getDiaLaboral() {
+        return diaLaboral;
+    }
+    
     
 }
